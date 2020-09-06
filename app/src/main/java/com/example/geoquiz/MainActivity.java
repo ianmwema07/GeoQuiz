58ec0dd8bf53085145baab163a4a762c1fc5e636
ianmwema07 <ianmwema07@gmail.com>
2020-09-06 12:05:14 +0300

    Just added some click listeners to the app

---
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.geoquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                //Does Nothing yet
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Does Nothing yet but soon
+
+            }
+        });
+    }
+}
